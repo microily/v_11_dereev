8e22d4abf54b91af91e49bd3de44134a83bed0ee
microily <danya.kejn@bk.ru>
2024-01-17 17:40:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dereev_v_11/setting.java
@@ -0,0 +1,4 @@
+package com.example.dereev_v_11;
+
+public class setting {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "dereev_v_11"
+include(":app")
